378ef90df76e68f73303245c3b8a42661f9b87a5
rtam <rtam@kbb.com>
2016-05-24 20:43:53 -0700

    working on precommit hooks

--- pre-commit.js
+++ pre-commit.js
@@ -1,14 +1,40 @@
-var runLinters = function (filesStaged) {
+var validateBranchName = function(branchName) {
     var statusCode = 1;
-    console.log(filesStaged);
+    console.log(branchName);
     statusCode = 0;
     return statusCode;
 }
 
+var checkForConflictMarkers = function (conflictMarkers) {
+    var statusCode = 1;
+    console.log(conflictMarkers);
+    statusCode = 0;
+    return statusCode;
+}
+
+var getLinterCommandArguments = function(filesStaged, extension) {
+    var filesSplit = filesStaged.split('\n');
+    var filteredFiles = [];
+    
+    for (var i = 0; i < filesSplit.length; i++)
+    {
+        var fileName = filesSplit[i];
+        // if there's no period in the filename, there is no extension
+        if (fileName.indexOf('.') > 0) {
+            var fileExt = fileName.substr(fileName.lastIndexOf('.') + 1);
+            if (fileExt === extension) {
+                filteredFiles.push(fileName);
+            }
+        }
+    }
+    return filteredFiles;
+}
+
 module.exports = {
-    validate: function (filesStaged) {
+    getLinterCommandArguments: getLinterCommandArguments,
+    validate: function (branchName, conflictMarkers) {
         var statusCode = 1;
-        if (runLinters(filesStaged) == 0) {
+        if (validateBranchName(branchName) == 0) { // && checkForConflictMarkers(conflictMarkers) == 0) {
             statusCode = 0;
         };
         return statusCode;
