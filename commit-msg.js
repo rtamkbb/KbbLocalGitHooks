51e72434508ce666f418b887381c56f167cd6e4e
rtam <rtam@kbb.com>
2016-05-24 23:32:58 -0700

    new version again

--- commit-msg.js
+++ commit-msg.js
@@ -1,15 +1,19 @@
 var validateText = function (msg) {
     var commitMessageRegexPattern = new RegExp('(Story: B-|BugId: )[0-9]* \| .*$');
     var match = commitMessageRegexPattern.test(msg);
+    var statusCode = 1;
     if (match) {
         console.log('PASS: Naming convention validation');
+        statusCode = 0;
     }
     else {
         console.log('ERROR: Naming convention validation');
     }
+    return statusCode;
 }
 
 var validateLength = function (msg) {
+    var statusCode = 1;
     if (!msg) {
         console.log('ERROR: No message');
     }
@@ -19,26 +23,33 @@ var validateLength = function (msg) {
     else if (msg.length > 50) {
         console.log('ERROR: Message too long');
     } else {
-        console.log('PASS: Message length passes')
+        console.log('PASS: Message length passes');
+        statusCode = 0;
     }
+    return statusCode;
 }
 
 var validateASCIICharacters = function (msg) {
+    var statusCode = 1;
     var ASCIIRegex = /^[ -~]+$/
     var isASCII = ASCIIRegex.test(msg);
     if (isASCII) {
         console.log('ERROR: Invalid characters detected');
     } else {
         console.log('PASS: No invalid characters detected');
+        statusCode = 0;
     }
+    return statusCode;
 }
 
 module.exports = {
     validate: function (msg) {
         console.log('Your commit message:');
+        var statusCode = 1;
         console.log(msg);
-        validateText(msg);
-        validateLength(msg);
-        validateASCIICharacters(msg);
+        if (validateText(msg) === 0 && validateLength(msg) === 0 && validateASCIICharacters(msg) === 0) {
+            statusCode = 0;
+        }
+        return statusCode;
     }
 };
